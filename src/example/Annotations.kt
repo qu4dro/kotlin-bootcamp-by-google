951ca6b758a1172aaa564db50a2cc3a11d09045e
qu4dro <33917440+qu4dro@users.noreply.github.com>
2021-01-07 12:58:08 +0800

    Task: Learn about annotations

---
+++ src/example/Annotations.kt
@@ -0,0 +1,40 @@
+package example
+
+import kotlin.reflect.full.declaredMemberFunctions
+import kotlin.reflect.full.findAnnotation
+
+annotation class ImPlant
+
+@Target(AnnotationTarget.PROPERTY_GETTER)
+annotation class OnGet
+@Target(AnnotationTarget.PROPERTY_SETTER)
+annotation class OnSet
+
+@ImPlant class Plant {
+    @get:OnGet
+    val isGrowing: Boolean = true
+
+    @set:OnSet
+    var needsFood: Boolean = false
+    
+    fun trim() {}
+    fun fertilize() {}
+}
+
+fun testAnnotations() {
+    val classObj = Plant::class
+    for (a in classObj.annotations) {
+        println(a.annotationClass.simpleName)
+    }
+}
+
+fun testAnnotations2() {
+    val plantObject = Plant::class
+    val myAnnotationObject = plantObject.findAnnotation<ImPlant>()
+    println(myAnnotationObject)
+}
+
+fun main() {
+    testAnnotations()
+    testAnnotations2()
+}
